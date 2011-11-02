240514c5d24a033ae0c2d85a826c3492bbfea5ad
KLIMaka <klimaka01@gmail.com>
2011-11-02 19:20:32 +0200

    remover PRO.Type enum

--- src/fdk/proto/PRO.java
+++ src/fdk/proto/PRO.java
@@ -1,82 +1,58 @@
 package fdk.proto;
 
-import java.security.InvalidParameterException;
 import java.util.Map;
 import java.util.TreeMap;
 
 public class PRO
 {
-    public enum Type
-    {
-        ITEM,
-        CRITTER,
-        SCENERY,
-        WALL,
-        TILE,
-        MISC,
-    }
+    public static final int             ITEM     = 0;
+    public static final int             CRITTER  = 1;
+    public static final int             SCENERY  = 2;
+    public static final int             WALL     = 3;
+    public static final int             TILE     = 4;
+    public static final int             MISC     = 5;
 
-    static private Map<Type, String> lstMap   = new TreeMap<Type, String>();
-    static private Map<Type, String> msgMap   = new TreeMap<Type, String>();
-    static private boolean           m_filled = false;
+    static private Map<Integer, String> lstMap   = new TreeMap<Integer, String>();
+    static private Map<Integer, String> msgMap   = new TreeMap<Integer, String>();
+    static private boolean              m_filled = false;
 
     static private void fillMaps()
     {
         if (m_filled == true)
             return;
 
-        lstMap.put(Type.ITEM, "ITEMS");
-        lstMap.put(Type.CRITTER, "CRITTERS");
-        lstMap.put(Type.SCENERY, "SCENERY");
-        lstMap.put(Type.MISC, "MISC");
-        lstMap.put(Type.TILE, "TILES");
-        lstMap.put(Type.WALL, "WALLS");
+        lstMap.put(ITEM, "ITEMS");
+        lstMap.put(CRITTER, "CRITTERS");
+        lstMap.put(SCENERY, "SCENERY");
+        lstMap.put(MISC, "MISC");
+        lstMap.put(TILE, "TILES");
+        lstMap.put(WALL, "WALLS");
 
-        msgMap.put(Type.CRITTER, "CRIT");
-        msgMap.put(Type.ITEM, "ITEM");
-        msgMap.put(Type.SCENERY, "SCEN");
-        msgMap.put(Type.MISC, "MISC");
-        msgMap.put(Type.TILE, "TILE");
-        msgMap.put(Type.WALL, "WALL");
+        msgMap.put(CRITTER, "CRIT");
+        msgMap.put(ITEM, "ITEM");
+        msgMap.put(SCENERY, "SCEN");
+        msgMap.put(MISC, "MISC");
+        msgMap.put(TILE, "TILE");
+        msgMap.put(WALL, "WALL");
 
         m_filled = true;
     }
 
-    static public Type intToType(int type)
-    {
-        switch (type) {
-        case 0:
-            return Type.ITEM;
-        case 1:
-            return Type.CRITTER;
-        case 2:
-            return Type.SCENERY;
-        case 3:
-            return Type.WALL;
-        case 4:
-            return Type.TILE;
-        case 5:
-            return Type.MISC;
-        default:
-            throw new InvalidParameterException("Invalid PROTO index");
-        }
-    }
-
-    static public String getLst(Type type)
+    static public String getLst(int type)
     {
         fillMaps();
         String t = lstMap.get(type);
         return "PROTO/" + t + "/" + t + ".LST";
     }
 
-    static public String getProDir(Type type)
+    static public String getProDir(int type)
     {
         fillMaps();
         String t = lstMap.get(type);
         return "PROTO/" + t + "/";
     }
 
-    static public String getMsg(Type type)
+    static public String getMsg(int type)
     {
         fillMaps();
         String t = msgMap.get(type);
